5d2aeb2b02f280795c7b656f8b29775ddd648f4b
unknown <l.lilinaipe@hotmail.com>
2025-06-18 18:30:33 -0300

    feat: criar services para User, task e project

---
+++ src/main/java/com/gustavo/taskflow/application/service/TaskService.java
@@ -0,0 +1,32 @@
+package com.gustavo.taskflow.application.service;
+
+import com.gustavo.taskflow.domain.model.Task;
+import com.gustavo.taskflow.domain.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> findAll(){
+        return taskRepository.findAll();
+    }
+
+    public Optional findbyId(long id){
+        return taskRepository.findById(id);
+    }
+
+    public Task save(Task task){
+        return taskRepository.save(task);
+    }
+
+    public void delete(long id){
+        taskRepository.deleteById(id);
+    }
+}
